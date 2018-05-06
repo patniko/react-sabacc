8898db70bd1e53d38cd54c8d18308c8cc344d99a
Evgeniy Poletaikin <v-evpole@microsoft.com>
2018-05-06 10:32:22 +0300

    10000 starting credits, 250 default bet

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export default ({
     mainPotAnteAmount: 50,
     sabaccPotAnteAmount: 50,
-    defaultBetAmount: 50,
-    initialPlayerBalance: 1000,
+    defaultBetAmount: 250,
+    initialPlayerBalance: 10000,
     playersCount: 2,
     numberOfPotBuildingRounds: 4,
     alertVisibilityTimeInMs: 2000,
